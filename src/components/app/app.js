b2092bf843d0cdf632c209d170807864f3550022
Aleksey Petrov <losyh2005@mail.ru>
2023-11-22 20:33:50 +0400

    Add filter function

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,11 +13,12 @@ class App extends Component {
         super(props)
         this.state = {
             data: [
-                {name: 'Lexa A.', salary: 800, increase: true, rise: false,  id: 1},
+                {name: 'Lexa A.', salary: 800, increase: true, rise: true,  id: 1},
                 {name: 'Oleg O.', salary: 3000, increase: false, rise: false,  id: 2},
                 {name: 'Vasya G.', salary: 5000, increase: true, rise: false,  id: 3}
             ],
-            term: ''
+            term: '',
+            filter: ''
         }
         this.maxId = 4
         this.increaseNumber = 0
@@ -70,11 +71,31 @@ class App extends Component {
     onUpdateSearch = (term) => {
         this.setState({term});
     }
+
+    filterPost = (items, filter) => {
+        switch(filter) {
+            case 'rise':
+                return items.filter(item => {
+                    return item.rise === true
+                })
+            case 'over' :
+                return items.filter(item => {
+                    return item.salary >= 1000
+                })
+            default:
+                return items
+        }
+    }
+
+    onUpdateFilter = (filter) => {
+        this.setState({filter})
+    }
+
     render() {
-        const {data, term} = this.state
+        const {data, term, filter} = this.state
         const increased = this.state.data.filter(item => item.increase).length
         const employees = this.state.data.length
-        const visibleData = this.seachEmp(data, term)
+        const visibleData = this.filterPost(this.seachEmp(data, term), filter)
         return (
             <div className="app">
                 <AppInfo
@@ -84,7 +105,7 @@ class App extends Component {
                 <div className="search-panel">
                     <SearchPanel
                     onUpdateSearch={this.onUpdateSearch}/>
-                    <AppFilter/>
+                    <AppFilter filter={filter} onUpdateFilter={this.onUpdateFilter}/>
                 </div>
     
                 <EmployersList 
